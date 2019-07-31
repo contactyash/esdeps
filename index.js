5ecf3394906e4bba37b566ea58648f52e0a38a9c
Yash Rathore <kanusdlp@gmail.com>
2019-07-31 22:32:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+var shell = require('shelljs');
+if (shell.exec("yarn add eslint eslint-plugin-react eslint-plugin-import eslint-import-resolver-webpack eslint-config-airbnb eslint-plugin-prettier eslint-config-prettier eslint-plugin-jsx-a11y -D").code !== 0) {
+  shell.echo('wow done');
+ } 
