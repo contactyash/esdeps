cb96648990321d0c5c2ce440f35ef0b443568221
Yash Rathore <kanusdlp@gmail.com>
2019-08-01 11:14:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ fs.copyFile("eslintrc.js", ".eslintrc", err => {
   if (err) console.log(err);
   console.log("SourceFile.txt was copied to DestinationFile.txt");
 });
-if (shell.cp("./eslintrc","/src").code !== 0) {
-  shell.echo('wow done');
- } 
 if (shell.exec("yarn add eslint eslint-plugin-react eslint-plugin-import eslint-import-resolver-webpack eslint-config-airbnb eslint-plugin-prettier eslint-config-prettier eslint-plugin-jsx-a11y -D").code !== 0) {
   shell.echo('wow done');
  } 
