d873129b4a0c1622283b2ecf6acb76c9a5482f34
Yash Rathore <kanusdlp@gmail.com>
2019-08-01 17:07:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 var sh = require('shelljs');
  
-
-});
 if (sh.exec("yarn add eslint eslint-plugin-react eslint-plugin-import eslint-import-resolver-webpack eslint-config-airbnb eslint-plugin-prettier eslint-config-prettier eslint-plugin-jsx-a11y -D").code !== 0) {
   sh.echo('wow done');
  } 
