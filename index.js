bd8ceab53dd9bc10d6740ebc795462c6b1e026d7
Yash Rathore <kanusdlp@gmail.com>
2019-08-01 11:30:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 var shell = require('shelljs');
 const fs = require("fs");
-fs.copyFile("eslintrc.js", ".eslintrc", err => {
-  if (err) console.log(err);
-  console.log("SourceFile.txt was copied to DestinationFile.txt");
+var fs = require('fs');
+ 
+fs.readFile('DATA', 'utf8', function(err, contents) {
+    console.log(contents);
 });
+// fs.copyFile("eslintrc.js", ".eslintrc", err => {
+//   if (err) console.log(err);
+//   console.log("SourceFile.txt was copied to DestinationFile.txt");
+// });
 // if (shell.exec("yarn add eslint eslint-plugin-react eslint-plugin-import eslint-import-resolver-webpack eslint-config-airbnb eslint-plugin-prettier eslint-config-prettier eslint-plugin-jsx-a11y -D").code !== 0) {
 //   shell.echo('wow done');
 //  } 
