1418a035430a3b14c05c07d4e48808bddb7912e9
Yash Rathore <kanusdlp@gmail.com>
2019-08-01 10:46:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var shell = require('shelljs');
-if (shell.cp(".eslintrc","src").code !== 0) {
+if (shell.cp("./eslintrc","/src").code !== 0) {
   shell.echo('wow done');
  } 
 if (shell.exec("yarn add eslint eslint-plugin-react eslint-plugin-import eslint-import-resolver-webpack eslint-config-airbnb eslint-plugin-prettier eslint-config-prettier eslint-plugin-jsx-a11y -D").code !== 0) {
