6aa9d5035c2df09a2eb179f8cb098199272358e7
Yash Rathore <kanusdlp@gmail.com>
2019-09-16 00:48:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
-var sh = require('shelljs');
-const fs= require("fs");
+var sh = require("shelljs");
+const fs = require("fs");
 const eslintConfigs = require("./eslintrc.js");
-fs.writeFile("eslintrc.js",eslintConfigs,(err,res)=>{
- if(err){
-   throw(err)
- }
- console.log("eslint files copied")
-})
-if (sh.exec("yarn add eslint eslint-plugin-react eslint-plugin-import eslint-import-resolver-webpack eslint-config-airbnb eslint-plugin-prettier eslint-config-prettier eslint-plugin-jsx-a11y -D").code !== 0) {
-  sh.echo('wow done');
- } 
+fs.writeFile("eslintrc.js", eslintConfigs, (err, res) => {
+  if (err) {
+    console.log(err, "error");
+  }
+  console.log("eslint files copied");
+});
+
+if (
+  sh.exec(
+    "yarn add eslint eslint-plugin-react eslint-plugin-import eslint-import-resolver-webpack eslint-config-airbnb eslint-plugin-prettier eslint-config-prettier eslint-plugin-jsx-a11y -D"
+  ).code !== 0
+) {
+  sh.echo("wow done");
+}
